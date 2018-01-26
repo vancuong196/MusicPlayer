c3b37e2ac47b3ec98f082c061272ba440afd6f4c
vancuong196 <vancuong196@gmail.com>
2018-01-26 22:41:28 +0700

    Initial commit

---
+++ app/src/main/java/player/project/com/musicplayer/MainActivity.java
@@ -0,0 +1,63 @@
+package player.project.com.musicplayer;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+
+    Button start,pause,stop;
+    Boolean t=false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start=(Button)findViewById(R.id.button1);
+        pause=(Button)findViewById(R.id.button2);
+        stop=(Button)findViewById(R.id.button3);
+        //creating media player
+
+        final MediaPlayer mp=new MediaPlayer();
+        try{
+            //you can change the path, here path is external directory(e.g. sdcard) /Music/maine.mp3
+            mp.setDataSource(Environment.getExternalStorageDirectory().getPath()+"/Music/maine.mp3");
+            mp.prepare();
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            public void onPrepared(MediaPlayer player) {
+                t=true;
+            }
+        });
+        start.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (t){
+                mp.start();}
+            }
+        });
+
+        pause.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.pause();
+            }
+        });
+        stop.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.stop();
+            }
+        });
+    }
+}
+
+
---
+++ app/src/main/java/player/project/com/musicplayer/player/Player.java
@@ -0,0 +1,8 @@
+package player.project.com.musicplayer.player;
+
+/**
+ * Created by Cuong on 1/26/2018.
+ */
+
+public class Player {
+}
